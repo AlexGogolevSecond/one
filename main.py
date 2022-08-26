4b80e3206bcec8290eab0eb4c0fdddb04a6cdf43
alex <inkeeper@inbox.ru>
2022-08-27 00:51:28 +0500

    second

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 from fastapi import FastAPI
 
+app = FastAPI()
+
+
+@app.get('/')
+def home():
+    return {'key': 'Hello!'}  #'Hello!'
+
+
+@app.get('/{pk}')
+def get_item(pk: int, q: str = None):
+    return {'key': pk, 'q': q}
